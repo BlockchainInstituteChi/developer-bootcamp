fa7579a5fc9c2602543dfc37d32684e12d20de78
alexander-morris <alex@theblockchaininstitute.org>
2019-12-02 14:37:34 -0600

    fixing gitignore

--- workshop_5_web3_and_decentralization/examples/simpleServer/node_modules/uport-credentials/src/index.ts
+++
@@ -1,5 +0,0 @@
-import Credentials from './Credentials'
-import { SimpleSigner } from 'did-jwt'
-import { ContractFactory } from './Contract'
-
-export { Credentials, SimpleSigner, ContractFactory }
