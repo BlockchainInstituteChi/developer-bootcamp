b89c73621823fde77d13a24095cfb6cf58d114cb
alexander-morris <alex@theblockchaininstitute.org>
2019-12-16 16:31:01 -0600

    fixing txId query

--- store/app/util/confirmationHelper.js
+++ store/app/util/confirmationHelper.js
@@ -10,13 +10,13 @@ var config = {
 	    user: 'test',
 	    pass: 'test',
 	    host: '127.0.0.1',
-	    port: '18443',
+	    port: '4000',
 	};
 
 var rpc  = new RpcClient(config);
 var zmq  = require('zeromq');
 var sock = zmq.socket('sub');
-var addr = 'tcp://127.0.0.1:3000';
+var addr = 'tcp://127.0.0.1:3001';
 
 
 // This module handles repetitive tasks associated with polling for confirmation results
@@ -34,8 +34,39 @@ module.exports = {
 			rpc.decodeRawTransaction(message.toString('hex'), function(err, resp) {
 				// Insert your transaction parsing logic here 
 				// i.e. for (pendingTx) ... if ( resp.address === pendingTx[i].address) then updateTxPaid('1234567890abcdefghijklmnopqrstuvwxyz');
+				if (err) return console.log('error decoding rpc raw tx', err);
+
+				console.log('rpc raw tx returned decoded', resp)
+
+				var outputs = resp.result.vout;
+
 				getPendingTx( function(err, result) {
 					console.log('found pending transactions ', result)
+
+
+			        for ( var vout in outputs ) {
+			        	vout = outputs[vout];
+
+					    if ( typeof(vout.scriptPubKey) != 'undefined'  ) {
+				            
+				        	if ( typeof(vout.scriptPubKey.addresses) != 'undefined'  ) {
+				        		console.log('vout addresses ', vout.scriptPubKey.addresses)
+
+					        	for ( var address in vout.scriptPubKey.addresses ) {
+
+					        		for ( var pendingTransaction in result ){
+						        		
+						        		console.log('checking pending transaction', pendingTransaction)
+					        			// Insert your comparison logic here to identify paid transactions
+					        			// Then run updateTxPaid( pendingTransaction._id )
+
+						        	}
+
+					        	}
+				        	}
+
+				        }
+				    }
 				});
 			});
 		});
@@ -56,19 +87,13 @@ module.exports = {
 	}
 }
 
-function updateTxPaid (paidAddress, paidAmount) {
+function updateTxPaid (txId) {
 
-	if (paidAmount === "undefined") {
-		var filter = { address : paidAddress };
-	} else {
-		var filter = { address : paidAddress, amount : paidAmount };
-	}
-
-	tx.updateMany(filter, { status : "paid" }, function(err, res) {
+	tx.updateMany({ _id : txId }, { status : "paid" }, function(err, res) {
 		
 		if (err) throw err;
 		
-		console.log('address ' + paidAddress + ' updated to paid ');
+		console.log('record updated to paid ', res);
 
 	});
 }
